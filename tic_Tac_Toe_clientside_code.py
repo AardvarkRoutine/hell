1444153139c3d86e527e54448d7b92a711441500
paulcmdhack <52427190+paulcmdhack@users.noreply.github.com>
2023-02-13 06:39:56 +0100

    ss

--- tic_Tac_Toe_clientside_code.py
+++ tic_Tac_Toe_clientside_code.py
@@ -1,14 +1,28 @@
 from tkinter import *
-import random as r
+import random as ra
+import time as t
+import requests as r
+import socket
+
 
 root = Tk()
-mainframe = Frame(root)
 root.title("9 Tic Tac Toe MP")
 root.geometry("590x600")
-def startscreen():
-    title = Label(mainframe, text="Tic Tac Toe Multiplayer", font=("Arial", 20))
+
+global gameID, sub_canvases, PlayerX, PlayerO, ip, notify
+gameID = 0
+sub_canvases = []
+PlayerX = False
+PlayerO = False
+ip = "http://localhost:5000"
+notify = Label(root, text="", font=("Arial", 15), wraplength=150)
+
+
+def startScreen():
+    global notify
+    title = Label(root, text="Tic Tac Toe Multiplayer", font=("Arial", 20))
     title.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
-    disp = Canvas(mainframe, width=400, height=400, bg="white")
+    disp = Canvas(root, width=400, height=400, bg="white")
 
     disp.create_line(0, 133, 400, 133, width=2)
     disp.create_line(0, 266, 400, 266, width=2)
@@ -16,25 +30,67 @@ def startscreen():
     disp.create_line(266, 0, 266, 400, width=2)
     disp.grid(row=1, column=0, rowspan=3, columnspan=3, padx=10, pady=10)
 
-    notify = Label(mainframe, text="Waiting for Player 2...", font=("Arial", 15), wraplength=150)
+    notify = Label(root, text="", font=("Arial", 15), wraplength=150)
     notify.grid(row=1, column=3, rowspan=3, columnspan=2, padx=10, pady=10)
 
-    new = Button(mainframe, text="New Game", font=("Arial", 15), command=newGame())
+    new = Button(root, text="New Game", font=("Arial", 15), command=newGame())
     new.grid(row=4, column=0, columnspan=5, padx=10, pady=10)
 
-    join = Button(mainframe, text="Join", font=("Arial", 15), command=joinGame())
+    join = Button(root, text="Join", font=("Arial", 15), command=joinGame())
     join.grid(row=5, column=0, columnspan=5, padx=10, pady=10)
 
+def clearScreen():
+    for widget in root.winfo_children():
+        if widget != notify:
+            widget.destroy()
+            notify.config(text="")
+
+def cancel():
+    clearScreen()
+    startScreen()
+
 def newGame():
-    pass
+    clearScreen()
+    gameID = r.post(ip + "/create_game").json()["game_id"]
+    PlayerX = True
+
+    code = Label(root, text="Game ID: " + str(gameID), font=("Arial", 15))
+    code.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
+    notify.grid(row=1, column=0, rowspan=3, columnspan=3, padx=10, pady=10)
+    cancel = Button(root, text="Cancel", font=("Arial", 15), command=cancel)
+    cancel.grid(row=4, column=0, columnspan=5, padx=10, pady=10)
+
+    game_full = False
+    while not game_full:
+        response = r.get(ip + "/game/" + str(gameID))
+        if response.status_code == 200:
+            game = response.json()
+            if game['player_o'] is not None:
+                game_full = True
+                clearScreen()
+                stdGameLoop()
+            else:
+                notify.config(text="Waiting for player O to join")
+                t.sleep(1)
+        else:
+            clearScreen()
+            startScreen()
+            notify.config(text="Game not found")
+            t.sleep(1)
+
+
+                
+
+    
 
 def joinGame():
     pass
 
 
 def stdGameLoop():
-    canvas = Canvas(mainframe, width=400, height=400)
-    canvas.pack()
+    global sub_canvas
+    canvas = Canvas(root, width=400, height=400)
+    canvas.pack() # NEED TO REMOVE IF IMPLEMENTING GRID
 
     for i in range(1, 3):
         x = i * 133.33
@@ -44,17 +100,16 @@ def stdGameLoop():
         y = i * 133.33
         canvas.create_line(0, y, 400, y, fill="black")
 
-    sub_canvases = []
     for i in range(3):
         for j in range(3):
             x1 = j * 133.33
             y1 = i * 133.33
             x2 = x1 + 133.33
             y2 = y1 + 133.33
-            print("x1: ", x1, "y1: ", y1, "x2: ", x2, "y2: ", y2)
-            sub_canvas = tk.Canvas(canvas, width=133.33, height=133.33, bd=0, highlightthickness=0,bg = r.choice(["red","blue","green","yellow","orange","purple","pink","black","white"]))
+            sub_canvas = Canvas(canvas, width=133.33, height=133.33, bd=0, highlightthickness=0, bg = ra.choice(["red", "blue", "green", "yellow", "orange", "purple", "pink", "black", "white"]))
             sub_canvas.place(x=x1, y=y1)
             sub_canvases.append(sub_canvas)
 
-stdGameLoop()
-mainframe.mainloop()
+startScreen()
+
+root.mainloop()
